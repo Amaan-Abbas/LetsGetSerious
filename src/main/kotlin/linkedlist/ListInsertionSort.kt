b0948c72695d1ca3a2c4b4b5356cae22db80d787
Amaan Abbas <amaan140904@gmail.com>
2025-10-06 15:34:54 +0530

    LeetCode 147

---
+++ src/main/kotlin/linkedlist/ListInsertionSort.kt
@@ -0,0 +1,23 @@
+package linkedlist
+
+fun insertionSortList(head: ListNode?): ListNode? {
+    if (head == null || head.next == null) return head //checks if the list is empty or has one node only
+
+    val fakeHead = ListNode(0) //fake head to serve as a sorted list in the linkedList
+    var current = head //current node
+    while (current != null) {
+        var preNode = fakeHead //sorted list
+        val nextNode = current!!.next //to store the next value in the original list
+
+        // condition to find the correct place of all the elements
+        while (preNode!!.next != null && preNode!!.next.`val` <= current!!.`val`) {
+            preNode = preNode!!.next
+        }
+
+        current!!.next = preNode!!.next // placing the node at the right place
+        preNode!!.next = current
+        current = nextNode
+    }
+
+    return fakeHead!!.next
+}
