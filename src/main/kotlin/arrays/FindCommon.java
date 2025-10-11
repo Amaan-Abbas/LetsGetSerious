0b76c7783c3d8644c47329a284aa3e72f5186a78
Amaan Abbas <amaan140904@gmail.com>
2025-10-11 16:13:45 +0530

    Complete them

--- src/main/kotlin/arrays/FindCommon.java
+++ src/main/kotlin/arrays/FindCommon.java
@@ -1,4 +1,33 @@
 package arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindCommon {
+
+    public static void main(String[] args) {
+        int answer1 = 0;
+        int answer2 = 0;
+
+        int[] arr = {2, 3, 2};
+        int[] arr2 = {1, 2};
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int j : arr) {
+            map.put(j, map.getOrDefault(j, 0) + 1);
+        }
+
+        Map<Integer, Integer> map2 = new HashMap<>();
+        for (int i : arr2) {
+            map2.put(i, map2.getOrDefault(i, 0) + 1);
+        }
+
+//        for (Map<Integer, Integer> Map) {
+//            if (map2.containsKey(i)) {
+//                answer1 = i
+//            }
+//        }
+
+
+    }
 }
--- src/main/kotlin/arrays/MaxValurArray.kt
+++ src/main/kotlin/arrays/MaxValurArray.kt
@@ -1,2 +1,17 @@
 package arrays
 
+//fun maxSumAfterPartitioning(arr: IntArray, k: Int): Int {
+//
+//}
+
+fun possibleCombinations(nums: IntArray): Int {
+    var possibleValues = 1
+    for (i in 1..nums.size) {
+        possibleValues *= i
+    }
+
+    return possibleValues
+}
+
+fun main() {
+}
