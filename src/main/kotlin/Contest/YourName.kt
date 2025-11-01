bd0e96a19a2d0d3ce5484d093a195e4273b42215
Amaan Abbas <amaan140904@gmail.com>
2025-11-01 14:39:09 +0530

    CodeForces Your Name

---
+++ src/main/kotlin/Contest/YourName.kt
@@ -0,0 +1,31 @@
+package Contest
+
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val input = sc.nextInt()
+
+    for (i in 1..input) {
+        val size = sc.nextInt()
+        val cubes = sc.next().toCharArray().sorted()
+        val name = sc.next().toCharArray().sorted()
+
+        println()
+
+        if (cubes.size != size || name.size != size) {
+            println("No")
+            break
+        }
+
+        for (i in 0 until size) {
+            if (cubes[i] != name[i]) {
+                println("No")
+                break
+            }
+
+            if (i == size - 1) println("Yes")
+        }
+    }
+}
