f869787e2b1ce5d11a691d38bf3c5e7e73c85b7f
Amaan Abbas <amaan140904@gmail.com>
2025-12-18 20:34:42 +0530

    Palindrome string

--- src/main/kotlin/recursion/StringPalindrome.java
+++ src/main/kotlin/recursion/StringPalindrome.java
@@ -1,4 +1,14 @@
 package recursion;
 
 public class StringPalindrome {
+    public static boolean palindromeString(int i, String str) {
+        int n = str.length();
+        if (i >= n / 2) return true;
+
+        return str.charAt(i) == str.charAt(n - i - 1) && palindromeString(i + 1, str);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindromeString(0, "11211"));
+    }
 }
