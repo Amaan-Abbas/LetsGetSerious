cffa1281223f86b751fe9a9c976b5f2c8d37f310
Amaan Abbas <amaan140904@gmail.com>
2026-02-09 14:17:26 +0530

    src/main/kotlin/Contest/JohnFarm.java

--- src/main/kotlin/Contest/JohnFarm.java
+++ src/main/kotlin/Contest/JohnFarm.java
@@ -1,4 +1,36 @@
 package Contest;
 
+import java.util.Scanner;
+
 public class JohnFarm {
+    public static int combination(int legs) {
+        int count = 0;
+        while (legs > 0) {
+            if (legs - 2 >= 0) {
+                count++;
+                legs -= 2;
+            }
+
+            if (legs - 4 >= 0) {
+                count++;
+                legs -= 4;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int legs = sc.nextInt();
+
+            if (legs % 2 == 0) {
+                int ans = combination(legs);
+                System.out.println(ans);
+            } else System.out.println(0);
+        }
+    }
 }
